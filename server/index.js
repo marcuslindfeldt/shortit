d4bd748eb5f5d56694cb19c2d79ae171f41a8de9
Marcus Lindfeldt <marcus.lindfeldt@gmail.com>
2019-01-17 20:39:19 +0100

    graphql server

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const { GraphQLServer } = require('graphql-yoga');
+const shortid = require('shortid');
+
+const typeDefs = `
+type Query {
+  tinyurl(id: ID!): TinyUrl
+}
+
+type Mutation {
+  createTinyUrl(url: String!): TinyUrl!
+}
+
+type TinyUrl {
+  id: ID!
+  url: String!
+}
+`;
+
+const urls = new Map();
+
+urls.set('Ag4HCA0K', {
+  id: 'Ag4HCA0K',
+  url: 'www.marcuslindfeldt.se',
+});
+
+const resolvers = {
+  Query: {
+    tinyurl: (parent, { id }) => urls.get(id),
+  },
+  Mutation: {
+    createTinyUrl: (parent, { url }) => {
+      const id = shortid();
+      const tinyurl = {
+        id,
+        url,
+      };
+
+      urls.set(id, tinyurl);
+
+      return tinyurl;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
